de1710732c2d0dc5a10d3ba09fa7fef42b47d9c1
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2026-06-20 14:55:26 +0600

    WalletCore: generic passkey attestation seam (for stable r1/Kernel)

--- packages/WalletCore/Sources/WalletCore/Models/Passkey.swift
+++ packages/WalletCore/Sources/WalletCore/Models/Passkey.swift
@@ -7,4 +7,10 @@ public struct Passkey {
     public let credentialID: Data
     public let name: String
     public let mnemonic: [String]
+
+    public init(credentialID: Data, name: String, mnemonic: [String]) {
+        self.credentialID = credentialID
+        self.name = name
+        self.mnemonic = mnemonic
+    }
 }
